a430bd2899a64af1e5203c153da2c2a9ee179bb1
FatimaAlmashhor <fatima.amin4official@gmail.com>
2023-05-02 17:31:16 +0300

    controllers , text , html ,relfation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,48 +1,67 @@
 import { useThree, useFrame, extend } from "@react-three/fiber"
+import { OrbitControls, TransformControls, PivotControls, Html } from '@react-three/drei'
 import { useRef } from 'react'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// extend({ OrbitControls })
 import CustomObject from "./CustomObject"
-extend({ OrbitControls })
 export default function Experience() {
     const cubeRef = useRef()
+    const sphare = useRef()
     const groupRef = useRef()
 
     // once it everythings is ready 
-    const { camera, gl } = useThree();
     // evey sigle frame
     useFrame((state, delta) => {
-        const angle = state.clock.elapsedTime
-        state.camera.position.x = Math.sin(angle) * 8
-        state.camera.position.z = Math.cos(angle) * 8
-        state.camera.lookAt(0, 0, 0)
+        // const angle = state.clock.elapsedTime
+        // state.camera.position.x = Math.sin(angle) * 8
+        // state.camera.position.z = Math.cos(angle) * 8
+        // state.camera.lookAt(0, 0, 0)
         // cubeRef.current.rotation.y += delta
         // groupRef.current.rotation.y += delta
     })
 
     return (
         <>
-            {/* <orbitControls args={[camera, gl.domElement]} /> */}
+            <OrbitControls makeDefault />
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
-            <group>
-                <group ref={groupRef}>
-                    <mesh position-x={- 2}>
+            <Html>Test</Html>
+            <group ref={groupRef}>
+                {/* PivotControl not working in group */}
+                <PivotControls
+                    anchor={[0, 1, 0]}
+                    depthTest={false}
+                    lineWidth={4}
+                    axisColors={['#9381ff', '#ff4d6d', '#7ae582']}
+                    scale={1}>
+                    <mesh position-x={- 2} ref={sphare}>
                         <sphereGeometry />
                         <meshStandardMaterial color="orange" />
+                        <Html
+                            position={[1, 1, 0]}
+                            wrapperClass="label"
+                            center
+                            distanceFactor={8}
+                            occlude={[sphare]}
+                        >
+                            That's a sphere 👍
+                        </Html>
                     </mesh>
+                </PivotControls>
 
-                    <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+                <TransformControls position-x={2} mode="rotate">
+                    <mesh scale={1.5}>
                         <boxGeometry />
                         <meshStandardMaterial color="mediumpurple" />
                     </mesh>
-                </group>
-
-                <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
-                    <planeGeometry />
-                    <meshBasicMaterial color="greenyellow" />
-                </mesh>
+                </TransformControls>
             </group>
-            <CustomObject />
+
+            <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
+                <planeGeometry />
+                <meshBasicMaterial color="greenyellow" />
+            </mesh>
+            {/* <CustomObject /> */}
         </>
     )
 }
