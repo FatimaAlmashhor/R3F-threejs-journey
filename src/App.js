0c5be088d4712026a503fd544de5abaee265bffe
FatimaAlmashhor <fatima.amin4official@gmail.com>
2023-05-02 16:49:16 +0300

    42: lasson

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber';
 import './App.css';
+import Experience from './Experience';
+import * as THREE from 'three'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      // flat
+      dpr={ [ 1, 2 ] } //pixel min max ratio control 
+      gl={{
+        antialias: true,
+        // toneMapping: THREE.ACESFilmicToneMapping
+      }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [3, 2, 6]
+      }}>
+      <Experience> </Experience>
+    </Canvas >
+
   );
 }
 
---
+++ src/Experience.jsx
@@ -0,0 +1,48 @@
+import { useThree, useFrame, extend } from "@react-three/fiber"
+import { useRef } from 'react'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import CustomObject from "./CustomObject"
+extend({ OrbitControls })
+export default function Experience() {
+    const cubeRef = useRef()
+    const groupRef = useRef()
+
+    // once it everythings is ready 
+    const { camera, gl } = useThree();
+    // evey sigle frame
+    useFrame((state, delta) => {
+        const angle = state.clock.elapsedTime
+        state.camera.position.x = Math.sin(angle) * 8
+        state.camera.position.z = Math.cos(angle) * 8
+        state.camera.lookAt(0, 0, 0)
+        // cubeRef.current.rotation.y += delta
+        // groupRef.current.rotation.y += delta
+    })
+
+    return (
+        <>
+            {/* <orbitControls args={[camera, gl.domElement]} /> */}
+            <directionalLight position={[1, 2, 3]} intensity={1.5} />
+            <ambientLight intensity={0.5} />
+            <group>
+                <group ref={groupRef}>
+                    <mesh position-x={- 2}>
+                        <sphereGeometry />
+                        <meshStandardMaterial color="orange" />
+                    </mesh>
+
+                    <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+                        <boxGeometry />
+                        <meshStandardMaterial color="mediumpurple" />
+                    </mesh>
+                </group>
+
+                <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
+                    <planeGeometry />
+                    <meshBasicMaterial color="greenyellow" />
+                </mesh>
+            </group>
+            <CustomObject />
+        </>
+    )
+}
